badf5015c3163e35ae186d1f00c30ff32e840a32
Felipe Marques <feelipemarquees@gmail.com>
2026-03-01 11:04:17 -0300

    Initial Commit

---
+++ linktrackr-backend/src/main/java/com/felipe/linktrackr_backend/LinktrackrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.linktrackr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinktrackrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinktrackrBackendApplication.class, args);
+	}
+
+}
---
+++ linktrackr-backend/src/test/java/com/felipe/linktrackr_backend/LinktrackrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.linktrackr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinktrackrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
